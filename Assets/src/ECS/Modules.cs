bb11decbed52b6a4087667f6d4ecc84453ddff2e
Cewein <pro.mnowak@gmail.com>
2020-07-09 11:00:44 +0200

    navmesh + games component + gameManager

---
+++ Assets/src/ECS/Modules.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Module
+{
+    public struct Edible : ECS.IComponent
+    {
+        bool active;
+    }
+
+    public struct FollowTarget : ECS.IComponent
+    {
+        GameObject target;
+    }
+
+    public struct TargetEdible : ECS.IComponent
+    {
+        GameObject target;
+    }
+
+    public struct Score : ECS.IComponent
+    {
+        int score;
+    }
+}
---
+++ Assets/src/Tradional/GameMananger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMananger : MonoBehaviour
+{
+    public int nbPecsMan;
+    public int nbFoods;
+    public int nbEnemy;
+
+    public GameObject PecsMan;
+    public GameObject Food;
+    public GameObject Enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
